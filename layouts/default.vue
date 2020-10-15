e377007a0c0f8d715f344eea169c4068985d3c67
Matija Jeras <matija.jeras64@gmail.com>
2020-10-15 23:43:55 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,55 @@
+<template>
+    <section style="display:flex;height:100vh;">
+        <b-sidebar
+            open
+            :fullheight="true"
+            position="static"
+            mobile="hide"
+        >
+            <div class="pl-4 pt-4 pr-4 pb-4">
+                <b-menu class="is-custom-mobile">
+                    <b-menu-list label="Navigation">
+                        <b-menu-item active icon="home" label="Home" @click="$router.push('/')"></b-menu-item>
+                        <b-menu-item icon="gamepad-variant" label="Play" @click="$router.push('/play')"></b-menu-item>
+                        <b-menu-item icon="trophy" label="Leaderboard"></b-menu-item>
+                    </b-menu-list>
+                    <b-menu-list label="Actions">
+                        <b-menu-item icon="login" label="Login"></b-menu-item>
+                    </b-menu-list>
+                </b-menu>
+            </div>
+            <!-- <nuxt-link :to="item.to" exact-active-class="is-active">
+                <b-icon :icon="item.icon" />
+                <span style="margin-left:8px;">{{ item.title }}</span>
+            </nuxt-link> -->
+        </b-sidebar>
+        <nuxt />
+    </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+            title: 'Home',
+            icon: 'home',
+            to: { name: 'index' }
+        },
+        {
+            title: 'Inspire',
+            icon: 'lightbulb',
+            to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+    html {
+        overflow: auto; 
+    }
+</style>
---
+++ pages/play.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="section px-8">
+        GAMEZ
+    </section>
+</template>
